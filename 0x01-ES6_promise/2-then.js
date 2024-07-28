a082de73ed20db2ae8df51bf962157f22c171075
joel magana <joelmagana15@gmail.com>
2024-07-28 14:52:53 +0000

    then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
 export function handleResponseFromAPI(promise) {
   return promise
-    .then((result) => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error(); // Returns an empty Error object
-    });
+    .then({status : 200, body : 'success'})
+    .catch(new Error(''));
+    .finally('Got a response from the API')};
 }
