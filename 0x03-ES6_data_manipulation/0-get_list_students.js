26ffa84876126338143ef038a3f49e58e9d791e8
joel magana <joelmagana15@gmail.com>
2024-07-30 12:04:22 +0000

    sec

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,20 @@
 export default function getListStudents() {
-  const first = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-
-  const second = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-
-  const third = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  return [first, second, third];
+	const first = {
+		id: 1,
+		firstName: 'Guillaume',
+		location: 'San Francisco',
+	};
+	const second = {
+		id: 2,
+		firstName: 'James',
+		location: 'Columbia',
+	};
+	const third = {
+		id: 5,
+		firstName: 'Serena',
+		location: 'San Francisco',
+	};
+	return [first, second, third];
 }
 
 
