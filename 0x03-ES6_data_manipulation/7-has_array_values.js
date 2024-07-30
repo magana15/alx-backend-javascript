66584d770c7d7248f663070f05011bea5dc44abf
joel magana <joelmagana15@gmail.com>
2024-07-30 13:15:36 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,5 @@
+//more sets
+export default function hasValuesFromArray(set, array) {
+	return array.every((value) => set.has(value));
+}
+
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,18 @@
+export default function cleanSet(set, startString) {
+	if (
+		!set && !startString && !(set instanceof Set) && typeof startString !== 'string') {
+		return '';
+	} //parts
+
+	const parts = [];
+	for (const value of set.values()) {
+		if (typeof value === 'string' && value.startsWith(startString)) {
+			const valueSubStr = value.substring(startString.length);
+			if (valueSubStr && valueSubStr !== value) {
+				parts.push(valueSubStr);
+				}
+		}
+	}
+	return parts.join('-');
+}
+
